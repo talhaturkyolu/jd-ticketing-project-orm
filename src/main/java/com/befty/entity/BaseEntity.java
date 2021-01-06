8c5c34599c467bcb7efa5eb2d94012244a50c6d5
Talha Turkyolu <turkyolutalha@gmail.com>
2021-01-06 11:54:40 -0600

    Ticketing Project - ORM

--- src/main/java/com/befty/entity/BaseEntity.java
+++ src/main/java/com/befty/entity/BaseEntity.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
@@ -27,6 +24,20 @@ public class BaseEntity {
     private Long lastUpdateUserId;
 
     private Boolean isDeleted = false;
+
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId  =1L;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId  =1L;
+    }
 
 
 }
